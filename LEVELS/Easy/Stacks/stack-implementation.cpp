7fa384801b196d8de879dd748a81ee26d4d740ea
sunitipaliwal <paliwalsuniti99@gmail.com>
2025-08-18 16:41:34 +0530

    stack-implementation

---
+++ LEVELS/Easy/Stacks/stack-implementation.cpp
@@ -0,0 +1,47 @@
+class MyStack
+{
+private:
+  queue<int> q;
+
+public:
+  MyStack()
+  {
+  }
+
+  void push(int x)
+  {
+    q.push(x);
+    int size = q.size();
+    for (int i = 1; i < size; i++)
+    {
+      q.push(q.front());
+      q.pop();
+    }
+  }
+
+  int pop()
+  {
+    int topper = q.front();
+    q.pop();
+    return topper;
+  }
+
+  int top()
+  {
+    return q.front();
+  }
+
+  bool empty()
+  {
+    return q.empty();
+  }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
