2b61fe8961b51931e8c7a3bf194429b00789c1fb
sunitipaliwal <paliwalsuniti99@gmail.com>
2025-08-22 19:39:28 +0530

    remove-nth-node

---
+++ MIX-remaining/Linked-List/remove-nthnode.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *removeNthFromEnd(ListNode *head, int n)
+  {
+    if (head == NULL || head->next == NULL)
+    {
+      return (n == 1) ? nullptr : head;
+    }
+    int count = 0;
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+      count++;
+      temp = temp->next;
+    }
+    if (count == n)
+    {
+      ListNode *newNode = head->next;
+      delete head;
+      return newNode;
+    }
+    int res = count - n;
+    temp = head;
+    while (temp != NULL)
+    {
+      res--;
+      if (res == 0)
+        break;
+      temp = temp->next;
+    }
+    ListNode *delNode = temp->next;
+    if (delNode != nullptr)
+    {
+      temp->next = delNode->next;
+      delete delNode;
+    }
+    return head;
+  }
+};
