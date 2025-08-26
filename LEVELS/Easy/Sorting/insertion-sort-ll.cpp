55e62b9e1b2d751b11447e6bd7358a27f7f5e3a1
sunitipaliwal <paliwalsuniti99@gmail.com>
2025-08-26 16:10:30 +0530

    insertion-sort-ll

---
+++ LEVELS/Easy/Sorting/insertion-sort-ll.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *insertionSortList(ListNode *head)
+  {
+    ListNode *dummy = new ListNode(10000);
+    while (head)
+    {
+      ListNode *temp = dummy;
+      ListNode *next = head->next;
+      while (temp->next && temp->next->val < head->val)
+      {
+        temp = temp->next;
+      }
+      head->next = temp->next;
+      temp->next = head;
+      head = next;
+    }
+    return dummy->next;
+  }
+};
